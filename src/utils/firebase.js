bd67023caef58d7fabf6c974285bc3d987025033
Mikee Morales <mikee-mbair@Mikees-Laptop.attlocal.net>
2024-06-17 17:26:18 -0700

    initial commit

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -3,13 +3,6 @@ import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  // apiKey: "AIzaSyBRvO29eG2dTcOBl5-36ilpFqk8gWYkl2k",
-  // authDomain: "trading-journal-bf221.firebaseapp.com",
-  // projectId: "trading-journal-bf221",
-  // storageBucket: "trading-journal-bf221.appspot.com",
-  // messagingSenderId: "176979770863",
-  // appId: "1:176979770863:web:4f4d4bd5058f5f43b90a1a",
-  // measurementId: "G-SJHHNPEPP6"
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
